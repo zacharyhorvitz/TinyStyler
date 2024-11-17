7ab04b5adb03311337c4d585332fdca790c2f37d
Zachary   Horvitz <zacharyhorvitz@gmail.com>
2024-11-17 18:49:55 -0500

    updated download paths, checked train logic

--- models/download.sh
+++ models/download.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-set -ex
+set -x
 
 # TinyStyler
 mkdir tinystyler
-#gdown FILL IN (EMNLP wifi slow) --fuzzy -O tinystyler/model_weights.pt
+gdown https://drive.google.com/file/d/17_nUvgnUVAdL6dmTQL0Nx6fKN7d1giY3/view?usp=sharing --fuzzy -O tinystyler/model_weights.pt
 
 # TinyStyler_Sim
 mkdir tinystyler_sim
-gdown https://drive.google.com/file/d/19gJJLoD3up4Nvkt_oY4XezgiNNyFD6Yh/view?usp=drive_link --fuzzy -O tinystyler_sim/model_weights.pt
+gdown https://drive.google.com/file/d/19gJJLoD3up4Nvkt_oY4XezgiNNyFD6Yh/view?usp=sharing --fuzzy -O tinystyler_sim/model_weights.pt
 
 # TinyStyler_Recon
 mkdir tinystyler_recon
